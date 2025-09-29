67e4fd3d04773d534a965832e849cb95f2beb1f4
ArfaxadRuiz <iron_arfa@hotmail.com>
2025-09-29 09:20:41 -0600

    se agregan mas funcionalidades a los templates con for y reutilizacion de codigo

--- product/views.py
+++ product/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def product_list(request):
+    products = [
+        {"name": "Laptop", "price": 999.99},
+        {"name": "Mouse", "price": 25.50},
+        {"name": "Teclado", "price": 45.75},
+        {"name": "Monitor", "price": 200.00},
+    ]
+    return render(request, "products.html", {"products": products})
